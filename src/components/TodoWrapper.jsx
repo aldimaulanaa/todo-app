e0d5ea99baf282776f32aecb148c6fe6ce9f030e
aldimaulanaa <32177263+aldimaulanaa@users.noreply.github.com>
2024-11-24 14:42:19 +0700

    Update TodoWrapper.jsx

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,80 +1,78 @@
-import React, { useEffect, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-import EditForm from "./EditForm";
-import TodoItem from "./TodoItem";
-import TodoForm from "./TodoForm"; 
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import EditForm from './EditForm';
+import TodoItem from './TodoItem';
+import TodoForm from './TodoForm';
 
 const TodoWrapper = () => {
-    const [todos, setTodos] = useState(() => {
-        return JSON.parse(localStorage.getItem("todo")) || [];
-    });
+  const [todos, setTodos] = useState(() => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+  });
 
-    const updateLocalStorage = (newTodos) => {
-        setTodos(newTodos);
-        localStorage.setItem("todo", JSON.stringify(newTodos));
-    };
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
-    const addTodo = (todo) => {
-        if (todos.length < 20) {  
-            const newTodo = [
-                {
-                    id: uuidv4(),
-                    task: todo,
-                    completed: false,
-                    isEditing: false,
-                },
-                ...todos  
-            ];
-            updateLocalStorage(newTodo);
-        } else {
-            alert("You can only add up to 20 todo items.");
-        }
-    };
+  const addTodo = (task) => {
+    if (todos.length < 20) {
+      const newTodo = {
+        id: uuidv4(),
+        task,
+        completed: false,
+        isEditing: false,
+      };
+      setTodos([newTodo, ...todos]);
+    } else {
+      alert('You can only add up to 20 todos.');
+    }
+  };
 
-    const deleteTodo = (id) => {
-        const newTodos = todos.filter((todo) => todo.id !== id);
-        updateLocalStorage(newTodos);
-    };
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
-    const toggleComplete = (id) => {
-        const newTodos = todos.map((todo) =>
-            todo.id === id ? { ...todo, completed: !todo.completed } : todo
-        );
-        updateLocalStorage(newTodos);
-    };
-
-    const editTodo = (id) => {
-        const newTodos = todos.map((todo) =>
-            todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
-        );
-        updateLocalStorage(newTodos);
-    };
-
-    const editTask = (id, task) => {
-        const newTodos = todos.map((todo) =>
-            todo.id === id ? { ...todo, task, isEditing: false } : todo
-        );
-        updateLocalStorage(newTodos);
-    };
-
-    return (
-        <div className="TodoWrapper">
-            <TodoForm addTodo={addTodo} />
-            {todos.map((todo) => 
-                todo.isEditing ? (
-                    <EditForm editTodo={editTask} task={todo} key={todo.id} />
-                ) : (
-                    <TodoItem 
-                        key={todo.id} 
-                        task={todo} 
-                        editTodo={editTodo} 
-                        deleteTodo={deleteTodo} 
-                        toggleComplete={toggleComplete} 
-                    />
-                )
-            )}
-        </div>
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
     );
+  };
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (id, newTask) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task: newTask, isEditing: false } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="TodoWrapper">
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo) =>
+        todo.isEditing ? (
+          <EditForm key={todo.id} editTodo={editTask} task={todo} />
+        ) : (
+          <TodoItem
+            key={todo.id}
+            task={todo}
+            editTodo={editTodo}
+            deleteTodo={deleteTodo}
+            toggleComplete={toggleComplete}
+          />
+        )
+      )}
+    </div>
+  );
 };
 
 export default TodoWrapper;
