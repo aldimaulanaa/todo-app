bf95af533fa26ab44a0f0d895048044669665229
aldimaulanaa <32177263+aldimaulanaa@users.noreply.github.com>
2024-11-24 14:41:36 +0700

    Update EditForm.jsx

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,27 +1,29 @@
 import React, { useState } from 'react';
 
 const EditForm = ({ editTodo, task }) => {
-    const [value, setValue] = useState(task.task);
+  const [value, setValue] = useState(task.task);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (value) {
-            editTodo(task.id, value);
-        }
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value.trim()) {
+      editTodo(task.id, value.trim());
+    }
+  };
 
-    return (
-        <form className="TodoForm" onSubmit={handleSubmit}>
-            <input
-                className="todo-input-edit"
-                type="text"
-                onChange={(e) => setValue(e.target.value)}
-                value={value}
-                placeholder="Edit your todo"
-            />
-            <button className="todo-btn-edit" type="submit">Save</button>
-        </form>
-    );
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        className="todo-input-edit"
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Edit your todo"
+      />
+      <button className="todo-btn-edit" type="submit">
+        Save
+      </button>
+    </form>
+  );
 };
 
 export default EditForm;
